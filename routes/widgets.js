b531598ccba45e8abc762cc00d69c486b68e54c2
Inno Irving V. Estrera <inno.irving.estrera@gmail.com>
2017-07-21 00:25:16 -0700

    json-server in progress

--- routes/widgets.js
+++ routes/widgets.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(req, res, next) {
-  res.render('widgets', {
-    title: 'Widgets'
+app.get('/', (req, res, next) => {
+  console.log(req.query);
+
+  res.send({
+    id: req.query.id,
+    name: 'inno',
+    description: 'inno was here',
+    price: 45.09
   });
 });
 
